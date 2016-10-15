7dcb913ad57ff51ed278fb8803237c417ea396d5
Jibola Sogbein <ajibolasogbein@gmail.com>
2016-10-15 20:53:58 +0100

    Add basic route and component for a gigs page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,9 +4,11 @@ import {Link, IndexLink} from 'react-router';
 const Header = () => {
     return (
         <nav>
-            <IndexLink to="/" activeClassName="active">Home </IndexLink>
-            {"|"}
-            <Link to="/about" activeClassName="active"> About</Link>
+            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+            {"  |  "}
+            <Link to="/about" activeClassName="active">About</Link>
+            {"  |  "}
+            <Link to="/gigs" activeClassName="active">Gigs</Link>
         </nav>
     );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import GigsPage from './components/gigs/GigsPage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="about" component={AboutPage}/>
+        <Route path="gigs" component={GigsPage}/>
     </Route>
 );
